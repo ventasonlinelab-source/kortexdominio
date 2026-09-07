46669172400d47bdc0f25380b67b5a6bfbe6444a
ventasonlinelab-source <ventasonlinelab@gmail.com>
2026-09-07 06:31:31 +0200

    Add A.R.I. PWA service worker

---
+++ ari/sw.js
@@ -0,0 +1,5 @@
+const CACHE='ari-v1';
+const ASSETS=['/ari/','/ari/index.html','/ari/manifest.webmanifest','/ari/icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r;}).catch(()=>caches.match(event.request).then(r=>r||caches.match('/ari/'))));});
