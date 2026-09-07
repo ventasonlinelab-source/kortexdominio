564b9f041bb8c352c2481404e81c93956d654d33
ventasonlinelab-source <ventasonlinelab@gmail.com>
2026-09-07 06:42:27 +0200

    Refresh A.R.I. cache after calendar sync snapshot

--- ari/sw.js
+++ ari/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ari-v2';
+const CACHE='ari-v3';
 const ASSETS=['/ari/','/ari/index.html','/ari/manifest.webmanifest','/ari/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
