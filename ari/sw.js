935f401093db24c991717fda7515e1236843e75a
ventasonlinelab-source <ventasonlinelab@gmail.com>
2026-09-07 07:36:54 +0200

    Bump A.R.I. cache for phase/task hierarchy

--- ari/sw.js
+++ ari/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ari-v10';
+const CACHE='ari-v11';
 const ASSETS=['/ari/','/ari/index.html','/ari/manifest.webmanifest','/ari/icon.svg','/ari/calendar.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
